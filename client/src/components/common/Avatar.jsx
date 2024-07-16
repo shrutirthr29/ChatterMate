9a04987228a03331427ff7cfde4b84511c4eb0a6
shrutirthr29 <shray.shruti@gmail.com>
2024-07-16 18:20:01 +0530

    avatar on onboarding page done

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Image from "next/image";
 
-function Avatar() {
-  return <div>Avatar</div>;
+function Avatar({type, image, setImage}) {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        {type==="sm" && (
+          <div className="relative h-10 w-10">
+            <Image src={image} alt="avatar" className="rounded-full" fill />
+          </div>
+        )}
+        {type==="lg" && (
+          <div className="relative h-14 w-14">
+            <Image src={image} alt="avatar" className="rounded-full" fill />
+          </div>
+        )}
+        {type==="xl" && (
+          <div className="relative h-60 w-60 cursor-pointer">
+            <Image src={image} alt="avatar" className="rounded-full" fill />
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Avatar;

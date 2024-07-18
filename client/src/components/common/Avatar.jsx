87170a8245730a10cb47898c65ef90b64c80b50c
shrutirthr29 <shray.shruti@gmail.com>
2024-07-18 16:16:57 +0530

    dropdown on change profile picture done

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import {FaCamera} from "react-icons/fa"
+import { FaCamera } from "react-icons/fa"
+import ContextMenu from "./ContextMenu";
 
 function Avatar({ type, image, setImage }) {
   const [hover, setHover] = useState(false)
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false)
+  const [contextMenuCoordinates, setContextMenuCoordinates] = useState({
+    x: 0,
+    y: 0,
+  })
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCoordinates({ x: e.pageX, y: e.pageY })
+    setIsContextMenuVisible(true);
+  }
+
+  const contextMenuOptions = [
+    {name : "Take Photo", callback: () => {}},
+    {name : "Choose From Library", callback: () => {}},
+    {name : "Upload Photo", callback: () => {}},
+    {name : "Remove Photo", callback: () => {}},
+  ]
   return (
     <>
       <div className="flex items-center justify-center">
@@ -19,21 +38,38 @@ function Avatar({ type, image, setImage }) {
         )}
         {type === "xl" && (
           <div className="relative cursor-pointer z-0"
-          onMouseEnter={()=>setHover(true)}
-          onMouseLeave={()=>setHover(false)}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
           >
-          <div className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
-          ${hover?"visible":"hidden"}
-          `}>
-            <FaCamera className="text-2xl" id="context-opener"/>
-            <span>Change Profile Photo</span>
-          </div>
+            <div className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
+          ${hover ? "visible" : "hidden"}
+          `}
+              onClick={(e) => showContextMenu(e)}
+              id="context-opener"
+            >
+              <FaCamera className="text-2xl"
+                id="context-opener"
+                onClick={(e) => showContextMenu(e)}
+              />
+              <span
+                id="context-opener"
+                onClick={(e) => showContextMenu(e)}
+              >Change Profile Photo</span>
+            </div>
             <div className="flex items-center justify-center h-60 w-60">
               <Image src={image} alt="avatar" className="rounded-full" fill />
             </div>
           </div>
         )}
       </div>
+      {isContextMenuVisible && (
+        <ContextMenu 
+          options={contextMenuOptions}
+          coordinates={contextMenuCoordinates}
+          contextMenu={isContextMenuVisible}
+          setContextMenu={setIsContextMenuVisible}
+        />
+      )}
     </>
   );
 }
--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({ options, coordinates, contextMenu, setContextMenu }) {
+  const contextMenuRef = useRef(null);
+  const handleClick = (e, callback) => {};
+  return (
+    <div className={`bg-dropdown-background fixed py-2 z-[100] shadow-xl`}
+      ref={contextMenuRef}
+      style={{
+        top: coordinates.y,
+        left: coordinates.x,
+      }}
+    >
+      <ul>{
+        options.map(({ name, callback }) => (
+          <li key={name} onClick={(e)=> handleClick(e, callback)}
+          className="px-5 py-2 cursor-pointer hover:bg-background-default-hover">
+            <span className="text-white">{name}</span>
+          </li>
+        ))
+      }
+      </ul>
+    </div>
+  )
 }
 
 export default ContextMenu;

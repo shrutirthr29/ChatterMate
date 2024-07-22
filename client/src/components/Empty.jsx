1d5446d1b1fba2597cd1f62322531044202a97c1
shrutirthr29 <shray.shruti@gmail.com>
2024-07-22 22:34:32 +0530

    empty component in the main page is done

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 function Empty() {
-  return <div>Empty</div>;
+  return (
+    <div className="border-panelbackground border-1 width-full bg-panelbackground flex flex-col h-[100vh] border-b-4 border-b-greenishblue items-center justify-center">
+      <Image src="/chattermate_gif.gif" alt="chattermate" height={300}  width={300}/>
+    </div>
+  );
 }
 
 export default Empty;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,7 +11,6 @@ module.exports = {
       },
       colors: {
         // secondary: "#8696a0",
-        "teal-light": "#7ae3c3",
         "photopicker-overlay-background": "rgba(30,42,49,0.8)",
         "dropdown-background": "#233138",
         // "dropdown-background-hover": "#182229",

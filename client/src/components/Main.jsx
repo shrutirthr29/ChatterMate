81a2020e57538b5e3860bbbaf5db9cf703703f8a
shrutirthr29 <shray.shruti@gmail.com>
2024-07-22 22:16:04 +0530

    main page layout done

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import ChatList from "./Chatlist/ChatList";
+import Empty from "./Empty";
 function Main() {
-  return <div>Main</div>;
+  return <>
+    <div className="grid grid-cols-main h-screen w-screen max-h-screen max-width-full overflow-hidden">
+      <ChatList />
+      <Empty />
+    </div>
+  </>;
 }
 
 export default Main;

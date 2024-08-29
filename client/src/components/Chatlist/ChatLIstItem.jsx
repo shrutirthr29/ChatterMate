42c176e9800b35ce4cf5b4ca66aec1f2252ee15e
shrutirthr29 <shray.shruti@gmail.com>
2024-08-29 15:16:40 +0530

    chatListItem name and about fetched

--- client/src/components/Chatlist/ChatLIstItem.jsx
+++ client/src/components/Chatlist/ChatLIstItem.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Avatar from "../common/Avatar";
+import { useStateProvider } from "@/context/StateContext";
 
 function ChatListItem({ data, isContactPage = false }) {
+  const handleContactClick = () => {
+
+  }
   return (
-    <div className={`flex cursor-pointer items-center hover:bg-search-input-container-background`}>
+    <div className={`flex cursor-pointer items-center hover:bg-search-input-container-background`}
+    onClick={handleContactClick}>
     <div className="min-w-fit px-5 pt-3 pb-1">
       <Avatar type="lg" image={data?.profilePicture}/>
+    </div>
+    <div className="min-h-full flex flex-col justify-center mt-3 pr2 w-full">
+      <div className="flex-justify-between">
+        <div>
+          <span className="text-greenishblue">{data?.name}</span>
+        </div>
+      </div>
+      <div className="flex border-b border-conversation-border pb-2 pt-1 pr-2">
+        <div className="flex justify-between w-full">
+          <span className="text-secondary line-clamp-1 text-sm">
+            {data?.about || "\u00A0"}
+          </span>
+        </div>
+      </div>
     </div>
     </div>
   );

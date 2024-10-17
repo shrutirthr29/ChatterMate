10d23c561d692c35fc9cb4c4f05067eba3eb4629
shrutirthr29 <shray.shruti@gmail.com>
2024-10-17 18:20:48 +0530

    incoming voice and video call

--- client/src/components/common/IncomingVideoCall.jsx
+++ client/src/components/common/IncomingVideoCall.jsx
@@ -1,12 +1,28 @@
+import { reducerCases } from "@/context/constants";
 import { useStateProvider } from "@/context/StateContext";
 import Image from "next/image";
 import React from "react";
 
 function IncomingVideoCall() {
-  const [{ incomingVideoCall }, dispatch] = useStateProvider()
+  const [{ incomingVideoCall, socket }, dispatch] = useStateProvider()
 
-  const acceptCall = () => { }
-  const rejectCall = () => { }
+  const acceptCall = () => {
+    dispatch({
+      type: reducerCases.SET_VIDEO_CALL,
+      videoCall: {...incomingVideoCall, type: "in-coming"}
+    })
+    socket.current.emit("accept-incoming-call", {id: incomingVideoCall.id})
+    dispatch({
+      type: reducerCases.SET_INCOMING_VIDEO_CALL,
+      incomingVideoCall: undefined,
+    })
+  }
+  const rejectCall = () => {
+    socket.current.emit("reject-video-call", {from: incomingVideoCall.id})
+    dispatch({
+      type: reducerCases.END_CALL
+    })
+  }
 
   return (
     <div className="h-24 w-80 fixed bottom-8 mb-0 right-6 z-50 rounded-sm flex gap-5 items-center justify-start p-4 bg-panelbackground text-black drop-shadow-2xl border-greenishblue border-2 py-14">

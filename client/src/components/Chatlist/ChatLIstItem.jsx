a3082fa9d7dd33c61634c77afdfa416dce3093aa
shrutirthr29 <shray.shruti@gmail.com>
2024-10-17 13:39:08 +0530

    sidebar contacts with notifications

--- client/src/components/Chatlist/ChatLIstItem.jsx
+++ client/src/components/Chatlist/ChatLIstItem.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import Avatar from "../common/Avatar";
 import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
+import { calculateTime } from "@/utils/CalculateTime";
+import MessageStatus from "../common/MessageStatus";
+import { FaCamera, FaMicrophone } from "react-icons/fa";
 
 function ChatListItem({ data, isContactPage = false }) {
   const [{userInfo, currentChatUser},dispatch]=useStateProvider();
   const handleContactClick = () => {
-    dispatch({type: reducerCases.CHANGE_CURRENT_CHAT_USER, user:{...data}})
-    dispatch({type: reducerCases.SET_ALL_CONTACTS_PAGE})
+    if(!isContactPage){
+      dispatch({
+        type: reducerCases.CHANGE_CURRENT_CHAT_USER,
+        user:{
+          name: data.name,
+          about: data.about,
+          profilePicture: data.profilePicture,
+          email: data.email,
+          id: userInfo.id === data.senderId ? data.receiverId : data.senderId,
+        }
+      })
+    }else{
+      dispatch({type: reducerCases.CHANGE_CURRENT_CHAT_USER, user:{...data}})
+      dispatch({type: reducerCases.SET_ALL_CONTACTS_PAGE})
+    }
   }
   return (
     <div className={`flex cursor-pointer items-center hover:bg-search-input-container-background`}
@@ -15,17 +31,50 @@ function ChatListItem({ data, isContactPage = false }) {
     <div className="min-w-fit px-5 pt-3 pb-1">
       <Avatar type="lg" image={data?.profilePicture}/>
     </div>
-    <div className="min-h-full flex flex-col justify-center mt-3 pr2 w-full">
-      <div className="flex-justify-between">
+    <div className="min-h-full flex flex-col justify-center mt-3 pr-2 w-full">
+      <div className="flex justify-between">
         <div>
           <span className="text-greenishblue">{data?.name}</span>
         </div>
+        {
+          !isContactPage && (
+            <div>
+              <span className={`${!data.totalUnreadMessages>0 ? "text-secondary" : "text-greenishblue"} text-sm`}>
+                {calculateTime(data.createdAt)}
+              </span>
+            </div>
+          )
+        }
       </div>
       <div className="flex border-b border-conversation-border pb-2 pt-1 pr-2">
         <div className="flex justify-between w-full">
           <span className="text-secondary line-clamp-1 text-sm">
-            {data?.about || "\u00A0"}
+            {isContactPage? data?.about || "\u00A0":
+            <div className="flex items-center gap-1 max-w-[200px]
+            sm:max-w-[250px] md:max-w-[300px] lg:max-w-[200px] xl:max-w-[300px]">
+
+            {
+              data.senderId === userInfo.id && (<MessageStatus messageStatus={data.messageStatus}/>)
+            }
+            {
+              data.type === "text" && (<span className="truncate">{data.message}</span>)
+            }
+            {
+              data.type === "audio" && (<span className="flex gap-1 items-center">
+              <FaMicrophone className="text-greenishblue" /> Audio
+              </span>)
+            }
+            {
+              data.type === "image" && (<span className="flex gap-1 items-center">
+              <FaCamera className="text-greenishblue" /> Image
+              </span>)
+            }
+            </div>
+            }
           </span>
+          {
+            data.totalUnreadMessages>0 && <span className="bg-greenishblue px-[5px] rounded-full text-sm text-white">{data.totalUnreadMessages}</span>
+          }
         </div>
       </div>
     </div>
--- client/src/components/Chatlist/List.jsx
+++ client/src/components/Chatlist/List.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import { reducerCases } from "@/context/constants";
+import { useStateProvider } from "@/context/StateContext";
+import axios from "axios";
+import React, { useEffect } from "react";
+import ChatListItem from "./ChatListItem";
+import { GET_INITIAL_CONTACTS_ROUTE } from "@/utils/ApiRoutes";
 
 function List() {
+
+  const [{userInfo, userContacts}, dispatch] = useStateProvider()
+  useEffect(()=>{
+    const getContacts = async()=>{
+      try {
+        const{
+          data: {users, onlineUsers},
+        }= await axios(`${GET_INITIAL_CONTACTS_ROUTE}/${userInfo.id}`)
+        dispatch({type:reducerCases.SET_ONLINE_USERS,onlineUsers})
+        dispatch({type:reducerCases.SET_USER_CONTACTS,userContacts:users})
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    if(userInfo?.id) getContacts()
+  },[userInfo])
   return (
     <div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar">
-    
+      {
+        userContacts.map((contact)=> (<ChatListItem data={contact} key={contact.id} />))
+      }
     </div>
   );
 }
